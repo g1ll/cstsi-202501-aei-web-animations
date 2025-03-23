c518e0576f20671e0341260e6d942cf25d443177
g1ll <gillvelleda@gmail.com>
2025-03-22 21:39:47 -0300

    refactor: demonstrando a geração dinamica de elementos com map

--- aula02-css-animations/animations-react/src/App.jsx
+++ aula02-css-animations/animations-react/src/App.jsx
@@ -4,15 +4,15 @@ import './styles/App.css'
 function App() {
 
   const links = [
-    { path: 'exemplo-01-animation', title: 'Exemplo 01: Propriedade Animation' },
-    { path: 'exemplo-02-abreviado', title: 'Exemplo 02: Animation Abreviada' },
-    { path: 'exemplo-03-porcentagens', title: 'Exemplo 03: Frames Intermediários' },
-    { path: 'exemplo-04-loader-imgs', title: 'Exemplo 04: Animação de Imagens' },
-    { path: 'exemplo-05-imgs-sprites', title: 'Exemplo 05: Animação com Sprites' },
-    { path: 'exemplo-06-animation-delays', title: 'Exemplo 06: Combinando Animações' },
-    { path: 'exemplo-07-delays-hover', title: 'Exemplo 07: Gatilho com :hover' },
-    { path: 'exemplo-08-imgs-hover', title: 'Exemplo 08: Gatilho e Imagens' },
-    { path: 'exemplo-09-skeleton', title: 'Exemplo 09: Loader Skeleton' },
+    { path: 'exemplo-01-animation', title: 'Propriedade Animation' },
+    { path: 'exemplo-02-abreviado', title: 'Animation Abreviada' },
+    { path: 'exemplo-03-porcentagens', title: 'Frames Intermediários' },
+    { path: 'exemplo-04-loader-imgs', title: 'Animação de Imagens' },
+    { path: 'exemplo-05-imgs-sprites', title: 'Animação com Sprites' },
+    { path: 'exemplo-06-animation-delays', title: 'Combinando Animações' },
+    { path: 'exemplo-07-delays-hover', title: 'Gatilho com :hover' },
+    { path: 'exemplo-08-imgs-hover', title: 'Gatilho e Imagens' },
+    { path: 'exemplo-09-skeleton', title: 'Loader Skeleton' },
   ]
 
   return (
@@ -22,14 +22,16 @@ function App() {
           <a className="is-success" target="_blank" href="#">Aula 02</a>
         </h1>
         <ul className="nes-list">
-          {links.map((item, index) => (
-            <li key={index}>
-              <Link to={`${item.path}`}>
-                <button type="button" className="nes-btn is-primary"> {'>>'} </button>
-                &nbsp;{item.title}
-              </Link>
-            </li>
-          ))}
+          {
+            links.map((item, index) => (
+              <li key={index}>
+                <Link to={item.path}>
+                  <button type="button" className="nes-btn is-primary"> {'>>'} </button>
+                  {` Exemplo 0${index+1} ${item.title}`}
+                </Link>
+              </li>
+            ))
+          }
         </ul>
         <div>
           <img src="assets/svgs/react.svg" alt="react" className="nes-avatar is-large logo react-spin" />
