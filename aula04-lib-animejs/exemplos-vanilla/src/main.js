f44d35d16c0ae1d510722a6dbb424be177233999
g1ll <gillvelleda@gmail.com>
2025-04-08 21:08:00 -0300

    feat: exemplos animejs em vanilla js

---
+++ aula04-lib-animejs/exemplos-vanilla/src/main.js
@@ -0,0 +1,41 @@
+import animeJsLogo from '/assets/img/animejs_logo.png?url'
+import {startAnimations} from './animations.js' 
+
+
+const links = [
+  { path: 'exemplo-01-intro-animejs', title: 'Introduação a Biblioteca' },
+  { path: 'exemplo-01-intro-animejs', title: 'Introduação a Biblioteca' },
+  { path: 'exemplo-01-intro-animejs', title: 'Introduação a Biblioteca' },
+  { path: 'exemplo-01-intro-animejs', title: 'Introduação a Biblioteca' },
+  { path: 'exemplo-01-intro-animejs', title: 'Introduação a Biblioteca' },
+]
+
+const DOMlist = document.querySelector('ul')
+links.forEach((item,index) =>{ 
+  let menuItem =  `Exemplo 0${index+1}-${item.title}`
+  let contentArray = menuItem.split('');
+  let content = contentArray.map((letter) => {
+    return `<span>${letter}</span>`
+  }).join('')
+  DOMlist.innerHTML += `
+  <li>
+    <a href="/pages/${item.path}/index.html">
+      <h3>${content}</h3>
+    </a>
+  </li>
+`})
+
+const DivImages = document.querySelector('div > img').parentElement
+console.log(DivImages)
+
+const linkAnimatecssDocs = document.createElement('a')
+linkAnimatecssDocs.href = 'https://animejs.com/documentation/getting-started'
+linkAnimatecssDocs.target = '_blank'
+
+const imgAnimeJsLogo = document.createElement('img')
+imgAnimeJsLogo.src = animeJsLogo;
+imgAnimeJsLogo.alt = 'AnimeJS Logo'
+imgAnimeJsLogo.classList.add('logoAnimeJs');
+imgAnimeJsLogo.onload = () => startAnimations()
+linkAnimatecssDocs.appendChild(imgAnimeJsLogo)
+DivImages.appendChild(linkAnimatecssDocs)
