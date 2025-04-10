6a8d3fed45c49d0659295349fe67fa2bf2e9dcde
g1ll <gillvelleda@gmail.com>
2025-04-10 10:01:08 -0300

    refactor: exemplo skeleton reactjs

--- aula02-css-animations/animations-react/src/pages/Exemplo09Skeleton/Exemplo09Skeleton.jsx
+++ aula02-css-animations/animations-react/src/pages/Exemplo09Skeleton/Exemplo09Skeleton.jsx
@@ -4,15 +4,16 @@ import ImageBoat from '/assets/img/image.jpg?url'
 
 const Exemplo09Skeleton = () => {
 
+  // eslint-disable-next-line no-unused-vars
   const [isLoaded, setIsLoaded] = useState(false);
-  const [LoadedData, setLoadedData] = useState({})
+  const [LoadedData, setLoadedData] = useState({});
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoaded(true);
+      // setIsLoaded(true);
       setLoadedData({ title: 'Exemplo Skeleton!', image: ImageBoat });
-    }, 5000); // 2 seconds delay
-    return () => clearTimeout(timer); 
+    }, 5000); 
+    return () => clearTimeout(timer);
   });
 
   return (
@@ -21,29 +22,43 @@ const Exemplo09Skeleton = () => {
       <ContainerExamples className="nes-container with-title" >
         <span className="title">Exemplo 9</span>
         <Container>
-          {!isLoaded ? <>
-            <Skeleton>
-              <h1></h1>
-            </Skeleton>
-            <Skeleton>
-              <div></div>
-            </Skeleton>
-          </>
-            :
-            <>
-              <h1>{LoadedData.title}</h1>
-              <div>
-                <img src={LoadedData.image} alt="barcos" width="200" height="200" />
-              </div>
-            </>
-          }
+          {!isLoaded
+            ? <SkeletonLayout />
+            : <ContentLayout
+                title={LoadedData.title}
+                image={LoadedData.image}
+              />}
         </Container>
       </ContainerExamples>
       <div>
         <a href="/">
-          <button type="button" title="Voltar ao início!" class="nes-btn is-error btn">Voltar</button>
+          <button type="button" title="Voltar ao início!" className="nes-btn is-error btn">Voltar</button>
         </a>
       </div>
+    </>
+  )
+}
+
+const ContentLayout = ({title,image}) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      <div>
+        <img src={image} alt="barcos" width="200" height="200" />
+      </div>
+    </>
+  )
+}
+
+const SkeletonLayout = () => {
+  return (
+    <>
+      <Skeleton>
+        <h1></h1>
+      </Skeleton>
+      <Skeleton>
+        <div></div>
+      </Skeleton>
     </>
   )
 }
