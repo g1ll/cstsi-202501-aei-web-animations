8365b1846e17a39c65d8f800c3fc759db7d6adc5
g1ll <gillvelleda@gmail.com>
2025-04-12 14:53:47 -0300

    feat: mudancas para deixar a animacao do mario-kart responsiva

--- aula04-lib-animejs/exemplos-vanilla/pages/exemplo-04-motion-path/script.js
+++ aula04-lib-animejs/exemplos-vanilla/pages/exemplo-04-motion-path/script.js
@@ -1,26 +1,55 @@
-import { animate, utils, svg} from 'https://cdn.jsdelivr.net/npm/animejs@4.0.0/+esm';
+import { animate, utils, svg } from 'https://cdn.jsdelivr.net/npm/animejs@4.0.0/+esm';
 const $ = utils.$;
 const $pathKart = svg.createMotionPath('.circuit svg path');
-const $marioKart =  $('#marioDiv')
-const $luigiKart =  $('#luigiDiv')
+const $marioKart = $('#marioDiv')
+const $luigiKart = $('#luigiDiv')
 
-const runLuigi = animate($luigiKart,{
-  ...$pathKart,
-  ease: 'outQuad',
-  duration: 3500,
-  delay:1000,
-  autoplay: false,
-});
+const runLuigi = {
+  anime:
+    animate($luigiKart, {
+      ...$pathKart,
+      ease: 'outQuad',
+      duration: 3500,
+      delay: 1000,
+      // zIndex: [{to:11, duration:100}, {to:13, duration:3400}],
+      autoplay: false,
+    })
+}
 
-const runMario  = animate(
-  $marioKart,
-  {
-  ...$pathKart,
-  ease: 'in',
-  duration: 3500,
-  autoplay: false,
-  onBegin: ()=>runLuigi.play(),
-});
+const runMario = {
+  anime: animate(
+    $marioKart,
+    {
+      ...$pathKart,
+      ease: 'in',
+      duration: 3500,
+      autoplay: false,
+      onBegin: () => runLuigi.anime.play(),
+    })
+}
 
-$marioKart[0].onclick = ()=>runMario.play();
+$marioKart[0].onclick = () => runMario.anime.play();
 // $luigiKart[0].onclick = ()=>runLuigi.play();
+
+window.onresize = () => {
+  console.log('resize');
+  const resizedPath = svg.createMotionPath('.circuit svg path');
+  runMario.anime = animate(
+    $marioKart,
+    {
+      ...resizedPath,
+      ease: 'in',
+      duration: 3500,
+      autoplay: false,
+      onBegin: () => runLuigi.anime.play(),
+    })
+
+  runLuigi.anime = animate($luigiKart, {
+    ...resizedPath,
+    ease: 'outQuad',
+    duration: 3500,
+    delay: 1000,
+    // zIndex: [11, 15],
+    autoplay: false,
+  })
+}
