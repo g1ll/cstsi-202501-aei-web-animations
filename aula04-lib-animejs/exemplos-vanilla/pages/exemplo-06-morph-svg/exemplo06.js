b2746e2cabd8ce8dd772cc7726cb9d24daa3a1cd
g1ll <gillvelleda@gmail.com>
2025-04-19 17:10:04 -0300

    fix: correcao em propriedades da animate e estrutura html

--- aula04-lib-animejs/exemplos-vanilla/pages/exemplo-06-morph-svg/exemplo06.js
+++ aula04-lib-animejs/exemplos-vanilla/pages/exemplo-06-morph-svg/exemplo06.js
@@ -5,11 +5,14 @@ const [$squarePath, $jsPath, $ecmaPath, $sPath] = $('#svg_js_logo path');
 
 const anime3 = animate($squarePath,  //alvo da animação (elemento svg
     {
-        // fill:[							//propriedade a ser modificada (fill)
-        // 	{value:'#ff0000', duration:1000},
+        //Desta forma podemos definir duração para cada keyframe
+        //  fill:[							//propriedade a ser modificada (fill)
+        // 	{value:'#ff0000', duration:1000}, 
         // 	{value:'#00ff00', duration:2000},
         // 	{value:'#0000ff', duration:2000}
         // ],
+
+        //Propriedade a ser modificada (fill) com duração igual para cada keyframe de acordo com a propriedade duration
         keyframes: [
             { fill: '#ff0000' },
             { fill: '#00ff00' },
@@ -17,11 +20,10 @@ const anime3 = animate($squarePath,  //alvo da animação (elemento svg
         ],
         duration: 1000, 			//duração em ms
         loop: true, 			//animação em loop (animation-count:infinite)
-        direction: 'alternate',
+        alternat:true,
         autoplay: false,
-        // easing: 'easeInOutExpo'	//função de tempo  (animation-timing-function)
-        // easing: 'linear'	//função de tempo  (animation-timing-function)
-        easing: 'easeInQuad'	//função de tempo  (animation-timing-function)
+        // ease: 'linear'	//função de tempo  (animation-timing-function)
+        ease: 'inQuad'	//função de tempo  (animation-timing-function)
     });
 
 const anime4 = animate($jsPath,  //alvo da animação (elemento svg
@@ -31,12 +33,10 @@ const anime4 = animate($jsPath,  //alvo da animação (elemento svg
             { d: svg.morphTo($ecmaPath), duration: 500 },
         ],
         loop: true, 			//animação em loop (animation-count:infinite)
-        direction: 'alternate',
         autoplay: false,
         alternate: true,
-        // easing: 'easeInOutExpo'	//função de tempo  (animation-timing-function)
-        // easing: 'linear'	//função de tempo  (animation-timing-function)
-        easing: 'easeInQuad'	//função de tempo  (animation-timing-function)
+        // ease: 'linear'	//função de tempo  (animation-timing-function)
+        ease: 'inQuad'	//função de tempo  (animation-timing-function)
     });
 
 $('#svg_js_logo')[0].onmouseenter = () => {
@@ -63,7 +63,7 @@ const anime5 = animate($pathsTargetsOdd,
         duration: 250,
         delay: 500,
         autoplay: false,
-        easing: 'linear',
+        ease: 'linear',
     });
 
 const anime6 = animate($pathTargetEven,
@@ -72,7 +72,7 @@ const anime6 = animate($pathTargetEven,
         duration: 500,
         delay: 250,
         autoplay: false,
-        easing: 'linear'
+        ease: 'linear'
     });
 
 $("#container-svg-3 svg")[0].onclick = () => {
