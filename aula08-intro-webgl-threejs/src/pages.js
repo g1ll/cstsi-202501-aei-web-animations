35323cc860e223ba1f64b2a0db4f09a5226d72f4
g1ll <gillvelleda@gmail.com>
2025-06-05 22:25:33 -0300

    feat: exemplos de threejs atualizados

---
+++ aula08-intro-webgl-threejs/src/pages.js
@@ -0,0 +1,6 @@
+export const pages = [
+  { path: '01-intro-threejs', title: 'Introdução a ThreeJS' },
+  { path: '02-texture-map', title: 'Aplicação de Texturas' },
+  // { path: '03-exemplo-swiper-slider', title: 'Swiper Slider' },
+  // { path: '04-exemplo-glide-slider', title: 'Glide Slider' }
+]
