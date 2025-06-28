41c0aec0d13ab19fd9c19c491d6f9e92934b72d9
g1ll <gillvelleda@gmail.com>
2025-06-27 23:12:20 -0300

    refactor: alterações de parâmetros para demonstrações em aula.

--- aula11-react-three-fiber/01-intro-react-three-fiber/src/App.jsx
+++ aula11-react-three-fiber/01-intro-react-three-fiber/src/App.jsx
@@ -6,11 +6,11 @@ function App() {
   return (
     <div id="canvas-container">
       <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="white" position={[0, 0, 5]} />
-        <pointLight color="white" position={[0, 2, 2]} intensity={1.5}/>
-        <mesh rotation-y={1}>
-          <boxGeometry args={[1, 1, 1]} />
+        <ambientLight intensity={2} />
+        <directionalLight  color="white" position={[0, 0, 5]} />
+        <pointLight color={0x00ff00} position={[0, 2, 2]} intensity={5}/>
+        <mesh rotation={[1,1,1]}>
+          <boxGeometry args={[3, 3, 3]} />
           <meshStandardMaterial color={0x00ff00}/>
         </mesh>
       </Canvas>
